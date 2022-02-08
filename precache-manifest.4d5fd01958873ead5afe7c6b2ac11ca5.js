7ebd6857e594c776d08bcb90bbfb68929311666d
NOUMAN AHSAN <fa18-bse-071@isbstudent.comsats.edu.pk>
2022-02-08 19:02:25 +0500

    second commitdds

---
+++ precache-manifest.4d5fd01958873ead5afe7c6b2ac11ca5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ecf78619e54a25edbd483bd56c7bfb",
+    "url": "/paymentform/index.html"
+  },
+  {
+    "revision": "7ba3d4fba59e1cab8755",
+    "url": "/paymentform/static/js/2.4dd02531.chunk.js"
+  },
+  {
+    "revision": "cc7f5cd04294823ed74e",
+    "url": "/paymentform/static/js/main.9b9a9823.chunk.js"
+  },
+  {
+    "revision": "c96a798eb93fc8a01480",
+    "url": "/paymentform/static/js/runtime~main.b5945f46.js"
+  }
+]);
