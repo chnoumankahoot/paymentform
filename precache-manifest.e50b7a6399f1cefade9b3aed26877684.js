26fcffc9196ba48562051d689b77941b164c7985
NOUMAN AHSAN <fa18-bse-071@isbstudent.comsats.edu.pk>
2022-02-08 18:18:35 +0500

    First commit

---
+++ precache-manifest.e50b7a6399f1cefade9b3aed26877684.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f17f1bfdd9f72730b4e6c435953c813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99cd6e5ecf91a103c81a",
+    "url": "/static/js/2.4504d702.chunk.js"
+  },
+  {
+    "revision": "cc7f5cd04294823ed74e",
+    "url": "/static/js/main.9b9a9823.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
